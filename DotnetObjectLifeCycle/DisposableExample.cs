392afafcec48c3832150ab9644737e6cb7c1da1f
madiyar <madiyar>
2022-10-16 03:19:42 +0600

    Init commit

---
+++ DotnetObjectLifeCycle/DisposableExample.cs
@@ -0,0 +1,90 @@
+﻿namespace DotnetObjectLifeCycle;
+
+
+
+class DisposeExample : IDisposable
+{
+    private bool disposed = false;
+
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (disposed)
+        {
+            return;
+        }
+
+        if (disposing)
+        {
+            //release managed resources
+        }
+        //release unmanaged resources
+    }
+
+    ~DisposeExample()
+    {
+        Dispose(false);
+    }
+}
+
+class CascadeDisposable : IDisposable
+{
+    private readonly Stream _stream;
+
+    
+    public CascadeDisposable(Stream stream)
+    {
+        _stream = stream;
+    }
+
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+            _stream.Dispose();
+        }
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+}
+
+class CascadeDisposableWithUnmanagedResources : IDisposable
+{
+    private Stream _stream;
+
+    private void ReleaseUnmanagedResources()
+    {
+        // TODO release unmanaged resources here
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        ReleaseUnmanagedResources();
+        if (disposing)
+        {
+            _stream.Dispose();
+        }
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    ~CascadeDisposableWithUnmanagedResources()
+    {
+        Dispose(false);
+    }
+}
---
+++ DotnetObjectLifeCycle/GarbageGeneration.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using static System.Console;
+
+class GarbageGeneration
+{
+    static void Main(string[] args)
+    {
+        OutputEncoding = Encoding.Unicode;
+        WriteLine("GC demonstration🚮");
+        WriteLine($"Max generation: {GC.MaxGeneration}");
+        GarbageHelper hlp = new GarbageHelper();
+        WriteLine($"Object generation: {GC.GetGeneration(hlp)}");
+        WriteLine($"Memory usage byte: {GC.GetTotalMemory(false)}");
+        hlp.MakeGarbage(); 
+        WriteLine($"Memory usage byte: {GC.GetTotalMemory(false)}");
+        WriteLine($"Memory usage byte: {GC.GetTotalMemory(false)}");
+        WriteLine($"Object generation: {GC.GetGeneration(hlp)}");
+    
+        GC.Collect();
+        WriteLine($"Memory usage byte: {GC.GetTotalMemory(false)}");
+        WriteLine($"Object generation: {GC.GetGeneration(hlp)}");
+        ReadKey();
+    }
+}
+
+
+
+
+
+
+class GarbageHelper
+{
+    public void MakeGarbage()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            Person p = new Person();
+        }
+    }
+
+    class Person
+    {
+        private string _name;
+        private string surname;
+        private byte age;
+    }
+}
